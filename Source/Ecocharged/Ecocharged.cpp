a680ab9b5b7b9a4d87da98db7be9f8dbb4093f5d
James Mills <jcmills.developer@gmail.com>
2025-04-25 17:45:09 +1000

    Initial commit

---
+++ Source/Ecocharged/Ecocharged.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ecocharged.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ecocharged, "Ecocharged" );
